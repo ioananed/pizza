bc42e023371aee6eeaa09b2d80d1d0cc8b4a1198
ioananed <ioananedelcu100@gmail.com>
2019-07-31 10:42:40 +0200

    changes

--- Script.js
+++ Script.js
@@ -24,6 +24,30 @@ takeOrder('pesto', 'thin');
 
 console.log(getTotal());
 
-function addToCard(){
-  
+var orderCount = 0;
+
+function Pepperoni(){
+  orderCount += 1;
+  orderCoun = orderCount * 7.5;
+  console.log( orderCount + ' Pizza de Queso y Pepperoni =' +  orderCoun + '€');
 }
+
+var orderCot = 0;
+
+function palmitosja(){
+  orderCot += 1;
+  orderCou = orderCount * 8;
+  console.log( orderCount + ' Pizza de Jamón y Palmitos =' +  orderCou + '€');
+}
+var ordercounte = orderCou + orderCoun;
+
+function getSubTotal(itemCount){
+  return itemCount *= ordercounte
+}
+function getTax() {
+  return getSubTotal(ordercounte)* 0.06;
+}
+function getTotal() {
+  return getSubTotal(ordercounte) + getTax();
+}
+console.log(getTotal());
